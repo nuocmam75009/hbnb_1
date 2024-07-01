8a4526fb0d0ea80d0c40d2985a27bc9efc973393
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-07-01 09:05:52 +0200

    initialize JWT app

--- solutions/solution-00/hbnb.py
+++ solutions/solution-00/hbnb.py
@@ -4,13 +4,24 @@ from src import create_app
 from flask_sqlalchemy import SQLAlchemy, create_engine
 from flask import Flask
 from sqlalchemy.orm import sessionmaker
-
-
-app = create_app()
-
-if __name__ == "__main__":
-    app.run()
+from flask_jwt_extended import JWTManager
+import secrets
+from src import create_app
+from flask_sqlalchemy import SQLAlchemy, create_engine
+from flask import Flask
+from sqlalchemy.orm import sessionmaker
+from flask_jwt_extended import JWTManager
+import secrets
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///development.db'
 db = SQLAlchemy(app)
+
+app = create_app()
+
+app.config['JWT_SECRET_KEY'] = secrets.token_urlsafe(16)
+print(app.config['JWT_SECRET_KEY'])
+jwt = JWTManager(app)
+
+if __name__ == "__main__":
+    app.run()
