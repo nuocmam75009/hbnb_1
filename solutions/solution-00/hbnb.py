5f5718e6ef06bb4f8bb6629d10b184891db1e05c
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-07-03 10:50:32 +0200

    chore: Update hbnb.py with environment variable configuration and database setup

--- solutions/solution-00/hbnb.py
+++ solutions/solution-00/hbnb.py
@@ -4,15 +4,27 @@ from flask_sqlalchemy import SQLAlchemy, create_engine
 from flask import Flask
 from flask_jwt_extended import JWTManager
 import secrets
+from dotenv import load_dotenv, dotenv_values
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
+app = create_app()
+
+# Set up a default secured SECRET_KEY if not provided by env
+app.config.setdefault('SECRET_KEY', secrets.token_urlsafe(32))
+
+# Config DB
 app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///development.db'
 db = SQLAlchemy(app)
 
-app = create_app()
+engine = create_engine(app.config['SQLALCHEMY_DATABASE_URL'])
+db = SQLAlchemy(app, engine=engine)
+app.config['DEBUG'] = False
 
-app.config['JWT_SECRET_KEY'] = secrets.token_urlsafe(16)
-print(app.config['JWT_SECRET_KEY'])
+# Config JWT
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY', secrets.token_urlsafe(32))
 jwt = JWTManager(app)
 
 if __name__ == "__main__":
