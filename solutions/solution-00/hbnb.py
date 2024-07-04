70ce3d7acc4379c2411b5514bc77d03eb0c83e52
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-07-04 11:15:58 +0200

    chore: Update hbnb.py with environment variable configuration and database setup

--- solutions/solution-00/hbnb.py
+++ solutions/solution-00/hbnb.py
@@ -1,31 +1,7 @@
 # Version: 1.0
 from src import create_app
-from flask_sqlalchemy import SQLAlchemy, create_engine
-from flask import Flask
-from flask_jwt_extended import JWTManager
-import secrets
-from dotenv import load_dotenv, dotenv_values
-import os
 
-load_dotenv()
-
-app = Flask(__name__)
 app = create_app()
-
-# Set up a default secured SECRET_KEY if not provided by env
-app.config.setdefault('SECRET_KEY', secrets.token_urlsafe(32))
-
-# Config DB
-app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///development.db'
-db = SQLAlchemy(app)
-
-engine = create_engine(app.config['SQLALCHEMY_DATABASE_URL'])
-db = SQLAlchemy(app, engine=engine)
-app.config['DEBUG'] = False
-
-# Config JWT
-app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY', secrets.token_urlsafe(32))
-jwt = JWTManager(app)
 
 if __name__ == "__main__":
     app.run()
--- solutions/solution-00/src/__init__.py
+++ solutions/solution-00/src/__init__.py
@@ -2,10 +2,34 @@
 
 from flask import Flask
 from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy, create_engine
+from src import create_app
+import os
+from flask_jwt_extended import JWTManager
+import secrets
+from dotenv import load_dotenv, dotenv_values
+
+load_dotenv()
 
 cors = CORS()
 db = SQLAlchemy()
+app = Flask(__name__)
+app = create_app()
+
+# Set up a default secured SECRET_KEY if not provided by env
+app.config.setdefault('SECRET_KEY', secrets.token_urlsafe(32))
+
+# Config DB
+app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///development.db'
+db = SQLAlchemy(app)
+
+engine = create_engine(app.config['SQLALCHEMY_DATABASE_URL'])
+db = SQLAlchemy(app, engine=engine)
+app.config['DEBUG'] = False
+
+# Config JWT
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY', secrets.token_urlsafe(32))
+jwt = JWTManager(app)
 
 def create_app(config_class="src.config.DevelopmentConfig") -> Flask:
     """
@@ -22,6 +46,8 @@ def create_app(config_class="src.config.DevelopmentConfig") -> Flask:
     register_handlers(app)
 
     return app
+
+
 
 
 def register_extensions(app: Flask) -> None:
